70c0389d1451075787d1dc69cb2a92b61da86f28
Fitz Ignacio <ignacio.fitz@gmail.com>
2022-11-25 21:33:47 -0800

    commit tree 7abae8a24f3166a3859aa189fa647c5fccd77c3a

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+   // Write your code here!
+   let result;
+    if (someValue <= 400) {
+      result = 'This one is on me!';
+    } else if (400 < someValue && someValue <= 2000) {
+      result = 'That will be twenty bucks.';
+    } else if (someValue > 2000 && someValue <= 2500) {
+      result = 'I will gladly take your thirty bucks.';
+    } else if (someValue >2500) {
+      result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  let results;
+  if (generosity === 'generous') {
+      results = 'Thank you so much.';
+  } else if (generosity === 'not as generous') {
+      results = 'Thank you.';
+  } else if (generosity === 'thanks for everything') {
+      results = 'Bye.';
+  }
+  return results
 }
